1e709ecd96a0cd777d11722c58a15f16af2c7047
Erish Prajapati <irishmjn@gmail.com>
2025-03-12 10:36:22 +0545

    ...

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+#lets start the real show
+class Profile(models.Model):
+    gender_choices = [
+        ("MALE" , 'male'),
+        ("FEMALE", 'female'),
+        ("OTHER", 'other')
+    ]
+    role_choices = [
+        ("ADMIN", 'admin'),
+        ("USER", 'user')
+    ]
+    
+    gender = models.CharField(max_length=10, choices = gender_choices, default = "MALE")
+    role = models.CharField(max_length=10, choice= role_choices, default = "USER")
+
+    def __str__(self):
+        return self.username
+    
+
+
+    
