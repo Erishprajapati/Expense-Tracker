f4f6d2d27d6e9ea8edb3c59f5123237dd90305a8
Erish Prajapati <irishmjn@gmail.com>
2025-03-12 13:11:30 +0545

    Postgres configured and adminr registered

--- backend/settings.py
+++ backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'expensetracker',  # Replace with your database name
+        'USER': 'postgres',  # Replace with your database username
+        'PASSWORD': '4696',  # Replace with your database password
+        'HOST': 'localhost',  # Or your PostgreSQL server address
+        'PORT': '5432',  # Default PostgreSQL port, change if necessary
     }
 }
 
